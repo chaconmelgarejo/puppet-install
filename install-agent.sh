c71584a9df62fe34f290ae854ab0b86e35923918
Alejandro JCM <chaconmelgarejo@gmail.com>
2017-07-06 14:28:26 -0400

    Create install-agent.sh

---
+++ install-agent.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+sudo dpkg -i puppetlabs-release-pc1-xenial.deb
+sudo apt-get update
+
+apt-get install puppet-agent
